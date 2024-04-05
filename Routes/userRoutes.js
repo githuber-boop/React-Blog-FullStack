1809f99f8e3d3d83ed6f1fe9f49c49722a1e8486
Jayce <ashishalex084@gmail.com>
2024-04-05 22:31:42 +0530

    Updataed

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -42,9 +42,5 @@ router.post('/api/login',async(req,res)=>{
     }
 })
 
-app.get('/*', function(req, res) 
-{ res.sendFile(path.join(__dirname ,'/build/index.html')); });
-
-
 
 module.exports = router;
--- app.js
+++ app.js
@@ -16,8 +16,13 @@ app.use(express.static(path.join(__dirname,'/build')));
 app.use(morgan('dev'));
 app.use(cors());
 
+app.get('/*', function(req, res) 
+{ res.sendFile(path.join(__dirname ,'/build/index.html')); });
+
 app.use('/api',userRoute)
 app.use('/api',postroute);
+
+
 
 
 
